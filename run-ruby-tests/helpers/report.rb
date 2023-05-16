526a92a10c3f9da133b65f59a883b89e73fa330a
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2023-05-16 18:56:49 -0400

    coverage all up in here

---
+++ run-ruby-tests/helpers/report.rb
@@ -0,0 +1,13 @@
+require "simplecov"
+require "simplecov-console"
+
+SimpleCov.collate(Dir["coverage/.resultset.json"], "rails") do
+  formatter SimpleCov::Formatter::Console
+
+  minimum_coverage 100
+  maximum_coverage_drop 2
+
+  add_group "Ignored Code" do |src_file|
+    File.readlines(src_file.filename).grep(/:nocov:/).any?
+  end
+end
