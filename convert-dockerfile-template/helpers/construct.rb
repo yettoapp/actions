b8239837fcc0eb70d4ab2f52434d7a1a4aec308a
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2024-10-29 16:51:25 -0400

    corrections

--- convert-dockerfile-template/helpers/construct.rb
+++ convert-dockerfile-template/helpers/construct.rb
@@ -3,21 +3,27 @@ require "bundler/inline"
 gemfile do
   source "https://rubygems.org"
 
-  gem "psych", "~> 5.2"
+  gem "psych", "~> 5.1"
   gem "json", "~> 2.7"
 end
 
 require 'psych'
 require 'json'
 
-if File.exists?("docker-values.yaml")
+if File.exist?("docker-values.yaml")
   values = Psych.load_file("docker-values.yaml")
 
   dockerfile_template = File.read("Dockerfile.template")
 
   values.each_pair do |key, value|
-    dockerfile_template.gsub!("%{#{key}}", value)
+    dockerfile_template.gsub!(/\# {{ #{key} }}/, value)
   end
+
+  puts <<~STR
+    Writing Dockerfile:
+
+    #{dockerfile_template}
+  STR
 
   File.write("Dockerfile", dockerfile_template)
 else
